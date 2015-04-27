cd3b413e4222464b61e05ebf9cb430d62ab7bd3c
Mark Ward <markmaw@gmail.com>
2015-04-27 13:42:38 -0400

    run from scratch binary

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/home/maw627/torch/install/bin/th main.lua -mode evaluate -format char -model char_baseline.net -vocab_size 50
+/scratch/maw627/torch/install/bin/th main.lua -mode evaluate -format char -model char_baseline.net -vocab_size 50
